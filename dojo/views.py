3ef3be9c8935c29c43cef4819c86a225af6cfa9f
kimsangwon <tkddnjs2014@gmail.com>
2019-07-18 17:16:55 +0900

    forth_commit

--- dojo/views.py
+++ dojo/views.py
@@ -1,6 +1,9 @@
 #dojo/views.py
-from django.http import HttpResponse, JsonResponse
+import os
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+
 
 def mysum(request, numbers):
     result= sum(map(lambda s: int(s or 0), numbers.split("/")))
@@ -29,3 +32,13 @@ def post_list3(request):
         'message': '안녕',
         'items': ['파이썬','메롱','azure'],
     }, json_dumps_params={'ensure_ascii': False})
+
+
+def excel_download(request):
+    #filepath= '/Users/sangwon/workspace/piro11_django/list.xlsx'
+    filepath= os.path.join(settings.BASE_DIR, 'list.xlsx')
+    filename= os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response= HttpResponse(f, content_type='application/vnd.ms-excel')
+        response['Content-Disposition']= 'attachment; filename="{}"'.format(filename)
+        return response
