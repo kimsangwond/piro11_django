2a3da753299c28c5e10f30ec787527df7985173b
kimsangwon <tkddnjs2014@gmail.com>
2019-07-18 22:44:44 +0900

    fifth_commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import re
+from django.forms import ValidationError
 
-# Create your models here.
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$',value):
+        raise ValidationError('invalid lnglat type')
+
+class Post(models.Model):
+    title=models.CharField(max_length=100, verbose_name='제목',help_text='포스팅 제목을 입력해주세요. 최대 100자 이내')
+    content = models.TextField(verbose_name='내용')
+    tags=models.CharField(max_length=100, blank=True)
+    lnglat= models.CharField(max_length=50, blank=True, validators=[lnglat_validator], help_text='경도/ 위도 포맷으로 입력')
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
