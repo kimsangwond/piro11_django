424a40738f2d7af898a4f9e9221cf273a6cc70e5
kimsangwon <tkddnjs2014@gmail.com>
2019-07-20 01:37:09 +0900

    eighth_commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+#admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content_size', 'status', 'created_at', 'updated_at']
+    actions=['make_draft','make_published']
+
+    def content_size(self, post):
+        return '{}글자'.format(len(post.content))
+    content_size.short_description ='글자'
+
+    def make_draft(self, request, queryset):
+        updated_count=queryset.update(status='d')
+        self.message_user(request, '{}건의 포스팅을 draft상태로 변경'.format(updated_count))
+
+
+    def make_published(self, request, queryset):
+        updated_count=queryset.update(status='p')
+        self.message_user(request, '{}건의 포스팅을 published상태로 변경'.format(updated_count))
--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,17 @@ def lnglat_validator(value):
         raise ValidationError('invalid lnglat type')
 
 class Post(models.Model):
+    STATUS_CHOICES=(
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
+
+    author=models.CharField(max_length=20)
     title=models.CharField(max_length=100, verbose_name='제목',help_text='포스팅 제목을 입력해주세요. 최대 100자 이내')
     content = models.TextField(verbose_name='내용')
     tags=models.CharField(max_length=100, blank=True)
     lnglat= models.CharField(max_length=50, blank=True, validators=[lnglat_validator], help_text='경도/ 위도 포맷으로 입력')
+    status=models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at= models.DateTimeField(auto_now_add=True)
     updated_at= models.DateTimeField(auto_now=True)
