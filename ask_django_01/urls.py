8a58a8c4f461858b15a0dc5283c98c3613a6bc93
kimsangwon <tkddnjs2014@gmail.com>
2019-07-21 18:23:14 +0900

    nineth_commit

--- ask_django_01/urls.py
+++ ask_django_01/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path, re_path
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
     url(r'^dojo/', include('dojo.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
--- blog/admin.py
+++ blog/admin.py
@@ -6,11 +6,12 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'content_size', 'status', 'created_at', 'updated_at']
-    actions=['make_draft','make_published']
+    actions=['make_draft', 'make_published']
 
     def content_size(self, post):
         return '{}글자'.format(len(post.content))
     content_size.short_description ='글자'
+
 
     def make_draft(self, request, queryset):
         updated_count=queryset.update(status='d')
